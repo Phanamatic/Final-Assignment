2dbf0d835b26e24efcc76617f2ea6c06769c19cc
Phanamatic <phanamatic@gmail.com>
2023-06-05 01:27:52 +0200

    UI Update

--- Ticket To Ride/Assets/Scripts/MenuButtonsController.cs
+++ Ticket To Ride/Assets/Scripts/MenuButtonsController.cs
@@ -8,7 +8,13 @@ public class MenuButtonsController : MonoBehaviour
 {
     public Button QuitBtn;
     public Button MenuBtn;
+    public Button RulesBtn;
+    public GameObject RulesPanel;
     public GameObject quitPromptPanel;
+    public GameObject menuPromptPanel;
+    public GameObject PlayerNumberPanel;
+    public GameObject PlayerAreaPanel;
+    public GameObject DecksAreaPanel;
 
     public void ReturnToMenu()
     {
@@ -20,6 +26,8 @@ public class MenuButtonsController : MonoBehaviour
         quitPromptPanel.SetActive(true);
         QuitBtn.gameObject.SetActive(false);
         MenuBtn.gameObject.SetActive(false);
+        RulesBtn.gameObject.SetActive(false);
+        PlayerNumberPanel.gameObject.SetActive(false);
     }
 
     public void QuitGameConfirm()
@@ -30,7 +38,41 @@ public class MenuButtonsController : MonoBehaviour
     public void QuitGameCancel()
     {
         quitPromptPanel.SetActive(false);
+        menuPromptPanel.SetActive(false);
         QuitBtn.gameObject.SetActive(true);
         MenuBtn.gameObject.SetActive(true);
+        RulesBtn.gameObject.SetActive(true); 
+        PlayerNumberPanel.gameObject.SetActive(true);
+    }
+
+    public void MenuPrompt()
+    {
+        menuPromptPanel.SetActive(true);
+        QuitBtn.gameObject.SetActive(false);
+        MenuBtn.gameObject.SetActive(false);
+        RulesBtn.gameObject.SetActive(false);
+        PlayerNumberPanel.gameObject.SetActive(false);
+    }
+
+    public void ShowRules()
+    {
+        PlayerAreaPanel.gameObject.SetActive(false);
+        DecksAreaPanel.gameObject.SetActive(false);
+        QuitBtn.gameObject.SetActive(false);
+        MenuBtn.gameObject.SetActive(false);
+        RulesBtn.gameObject.SetActive(false);
+        PlayerNumberPanel.gameObject.SetActive(false);
+        RulesPanel.gameObject.SetActive(true);   
+    }
+
+    public void CloseRules()
+    {
+        PlayerAreaPanel.gameObject.SetActive(true);
+        DecksAreaPanel.gameObject.SetActive(true);
+        QuitBtn.gameObject.SetActive(true);
+        MenuBtn.gameObject.SetActive(true);
+        RulesBtn.gameObject.SetActive(true); 
+        PlayerNumberPanel.gameObject.SetActive(true);
+        RulesPanel.gameObject.SetActive(false);
     }
 }
